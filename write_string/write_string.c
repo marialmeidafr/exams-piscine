e2494c0d189a09439bb1deecb202df49eca4acd0
gthuanny <gabithuanny1@gmail.com>
2025-09-15 10:03:11 +0100

    Add files via upload

--- write_string/write_string.c
+++ write_string/write_string.c
@@ -1,18 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   write_string.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gde-cast <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/13 16:16:01 by gde-cast          #+#    #+#             */
+/*   Updated: 2025/09/15 10:02:11 by gde-cast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void write_string(char *str)
+void	write_string(char *str)
 {
-	int  i = 0;
+	int	i;
 
-	while(str[i])
+	i = 0;
+	while (str[i] != '\0')
 	{
 		write(1, &str[i], 1);
 		i++;
 	}
 }
 
-int main(void)
+int	main(void)
 {
-	char str[] = "teste";
+	char str[] = "escrevastring"; 
 	write_string(str);
 }
